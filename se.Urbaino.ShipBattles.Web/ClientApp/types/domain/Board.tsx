b7f74b916667c2f73fb2377ef50c56a651c0d234
Martin Larsson <martin.larsson.6@gmail.com>
2018-08-19 18:45:14 +0200

    Ships are now separate svg-elements

--- se.Urbaino.ShipBattles.Web/ClientApp/types/domain/Board.tsx
+++ se.Urbaino.ShipBattles.Web/ClientApp/types/domain/Board.tsx
@@ -4,8 +4,8 @@ import * as React from "react";
 import { RouteComponentProps } from "react-router";
 
 interface BoardProps {
-    board : BoardState,
-    clickCallback : Function
+    board: BoardState,
+    clickCallback: Function
 }
 
 export interface BoardState {
@@ -16,51 +16,51 @@ export interface BoardState {
 }
 
 export default function Board(props: BoardProps) {
-    enum CellState { Ship, Shot, ShotShip };
+    enum CellState { Shot, ShotShip };
+
+    var cellWidth = 25;
+    var cellPadding = 3;
 
     var board: BoardState = props.board;
+
+    let ships: JSX.Element[] = [];
+    board.ships.forEach((ship: Ship) => {
+        var x = (ship.origin.x * cellWidth) + cellPadding;
+        var y = (ship.origin.y * cellWidth) + cellPadding;
+        var shipWidth: number = 0;
+        var shipHeight: number = 0;
+        switch (ship.heading) {
+            case Direction.North:
+                y -= (ship.length - 1) * cellWidth;
+            case Direction.South:
+                shipWidth = cellWidth - 2 * cellPadding;
+                shipHeight = (ship.length * cellWidth) - 2 * cellPadding;
+                break;
+            case Direction.West:
+                x -= (ship.length - 1) * cellWidth;
+            case Direction.East:
+                shipHeight = cellWidth - 2 * cellPadding;
+                shipWidth = (ship.length * cellWidth) - 2 * cellPadding;
+                break;
+        }
+        ships.push(<rect key={`${ship.origin.x};${ship.origin.y}-ship`} x={x} y={y} width={shipWidth} height={shipHeight} className={`ship`}></rect>);
+    });
 
     let items: CellState[][] = [];
     for (let i = 0; i < board.width; i++) {
         items[i] = [];
     }
 
-    board.ships.forEach((ship: Ship) => { // NOTE: Dictionary would be nice
-        var x = ship.origin.x;
-        var y = ship.origin.y;
-        for (let length = 0; length < ship.length; length++) {
-            items[x][y] = CellState.Ship;
-            switch (ship.heading) {
-                case Direction.North:
-                    y -= 1;
-                    break;
-                case Direction.East:
-                    x += 1;
-                    break;
-                case Direction.West:
-                    x -= 1;
-                    break;
-                case Direction.South:
-                    y += 1;
-                    break;
-            }
-        }
-    });
-
     board.shots.forEach((shot: ShotModel) => {
         var value: CellState = shot.hit ? CellState.ShotShip : CellState.Shot;
         items[shot.coordinates.x][shot.coordinates.y] = value;
     });
-
 
     let cells = [];
     for (let y = 0; y < board.height; y++) {
         for (let x = 0; x < board.width; x++) {
             let state: string = '';
             switch (items[x][y]) {
-                case CellState.Ship:
-                    state = 'ship';
-                    break;
                 case CellState.Shot:
                     state = 'shot';
                     break;
@@ -70,13 +70,14 @@ export default function Board(props: BoardProps) {
                 default:
                     break;
             }
-            cells.push(<rect key={`${x};${y}`} x={x*25} y={y*25} className={`boardCell ${state}`} onClick={() => props.clickCallback(x, y)}></rect>);
+            cells.push(<rect key={`${x};${y}`} x={x * cellWidth} y={y * cellWidth} height={cellWidth} width={cellWidth} className={`boardCell ${state}`} onClick={() => props.clickCallback(x, y)}></rect>);
         }
     }
 
     return (
         <svg className="board">
             {cells}
+            {ships}
         </svg>
     );
 } 
